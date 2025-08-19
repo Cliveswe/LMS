d6d7426113ff0b43a1dc49ed73ce663f10f09275
Clive-Leddy <clive.leddy@gmail.com>
2025-08-19 14:13:39 +0200

    Created a controller class for course.

---
+++ LMS.Presentation/Controllers/CourseControllers/CoursesController.cs
@@ -0,0 +1,11 @@
+﻿//Ignore Spelling: api
+using Microsoft.AspNetCore.Mvc;
+using Service.Contracts;
+
+namespace LMS.Presentation.Controllers.CourseControllers;
+
+[Route("api/course")]
+[ApiController]
+public class CoursesController(IServiceManager serviceManager) : Controller
+{
+}
