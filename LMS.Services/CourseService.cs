08bf99526fc36b8a045b2bf4d14a0f44afbebcdb
Clive-Leddy <clive.leddy@gmail.com>
2025-08-19 14:37:05 +0200

    Created course service interface. Added method AddCourse.

---
+++ LMS.Services/CourseService.cs
@@ -0,0 +1,13 @@
+﻿//Ignore Spelling: dto
+using AutoMapper;
+using Domain.Contracts.Repositories;
+using LMS.Shared.DTOs.CourseDtos;
+
+namespace LMS.Services;
+public class CourseService(IMapper mapper, IUnitOfWork unitOfWork) : ICourseService
+{
+    public void AddCourse(CourseCreateDto courseCreateDto)
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ Service.Contracts/ICourseService.cs
@@ -0,0 +1,9 @@
+﻿using LMS.Shared.DTOs.CourseDtos;
+
+namespace LMS.Services;
+
+public interface ICourseService
+{
+
+    void AddCourse(CourseCreateDto courseCreateDto);
+}
