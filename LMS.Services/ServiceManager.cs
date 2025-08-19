edf40047be25dc9205fc8fcaea9ce623774a1a4b
Clive-Leddy <clive.leddy@gmail.com>
2025-08-19 14:47:48 +0200

    Added CourseService to the ServiceManager.

--- LMS.Services/ServiceManager.cs
+++ LMS.Services/ServiceManager.cs
@@ -7,8 +7,16 @@ public class ServiceManager : IServiceManager
     private Lazy<IAuthService> authService;
     public IAuthService AuthService => authService.Value;
 
-    public ServiceManager(Lazy<IAuthService> authService)
+    //Course Service
+    private Lazy<ICourseService> courseService;
+    public ICourseService CourseService => courseService.Value;
+
+    public ServiceManager(
+        Lazy<IAuthService> authService,
+        Lazy<ICourseService> courseService
+        )
     {
         this.authService = authService;
+        this.courseService = courseService;
     }
 }
