15f602b7b6b0846ef420da808c2e1024bcbdfa1f
Keshti Gyllinger <gyllinger@gmail.com>
2025-08-20 14:39:35 +0200

    Added DTO for reading Modules

---
+++ LMS.Shared/DTOs/ModuleDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LMS.Shared.DTOs;
+
+public record ModuleDto
+{
+    public int Id { get; init; }
+    public string Name { get; init; }
+    public string Description { get; init; }
+    public DateTime StartDate { get; init; }
+    public DateTime EndDate { get; init; }
+}
