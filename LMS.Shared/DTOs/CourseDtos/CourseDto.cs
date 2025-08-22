30309645f454b8a8d95af29b7b8dd68f66b6b02c
Clive-Leddy <clive.leddy@gmail.com>
2025-08-22 09:19:43 +0200

    Specified the data filed values required for CourseDto. Specified the property names to be presented in the JSON when serializing and deserializing.

--- LMS.Shared/DTOs/CourseDtos/CourseDto.cs
+++ LMS.Shared/DTOs/CourseDtos/CourseDto.cs
@@ -1,8 +1,20 @@
 ﻿//Ignore Spelling: dto
+using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
+
 namespace LMS.Shared.DTOs.CourseDtos;
 public class CourseDto
 {
+
+    [Required(ErrorMessage = "CourseName is a required field.")]
+    [JsonPropertyName("CourseName")]
     public string CourseName { get; set; }
+
+    [Required(ErrorMessage = "CourseDescription is a required field.")]
+    [JsonPropertyName("CourseDescription")]
     public string CourseDescription { get; set; }
+
+    [Required(ErrorMessage = "CourseStartDate is a required field.")]
+    [JsonPropertyName("CourseStartDate ")]
     public DateTime CourseStartDate { get; set; }
 }
